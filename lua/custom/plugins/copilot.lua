11bd5bd0ec6a5891a30fe8364c131c57b79bfc54
trog <admin@trog.host>
2024-03-17 22:34:34 -0700

    disable copilot

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,6 +1,6 @@
 return {
-  'zbirenbaum/copilot.lua',
-  cmd = 'Copilot',
-  event = 'InsertEnter',
-  opts = { suggestion = { auto_trigger = true, debounce = 150 }, { filetypes = { ['*'] = true } } },
+  -- 'zbirenbaum/copilot.lua',
+  -- cmd = 'Copilot',
+  -- event = 'InsertEnter',
+  -- opts = { suggestion = { auto_trigger = true, debounce = 150 }, { filetypes = { ['*'] = true } } },
 }
