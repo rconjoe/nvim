0764076e8bc5ce5e042d074f35fa6e25d18a5301
trog <admin@trog.host>
2024-03-17 22:35:01 -0700

    folds <3

--- init.lua
+++ init.lua
@@ -16,6 +16,8 @@ vim.g.have_nerd_font = false
 vim.opt.number = true
 vim.opt.relativenumber = true
 
+vim.opt.foldmethod = 'indent'
+
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
 
