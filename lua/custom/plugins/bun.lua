eff9644b93675796a00abb36def7dc498b52d7ea
trog <admin@trog.host>
2024-03-23 16:56:14 -0700

    return empty struct

--- lua/custom/plugins/bun.lua
+++ lua/custom/plugins/bun.lua
@@ -1,16 +1,16 @@
--- return {
---   'LowByteFox/bun.nvim',
---   dependencies = {
---     'nvim-treesitter/nvim-treesitter',
---     'akinsho/toggleterm.nvim'
---   },
---   config = function()
---     require('bun').setup({
---       close_on_exit = true | false, -- if the terminal window should close instantly after bun exited
---       cwd = "current" | "relative", -- run_current's working directory
---       -- if "current" it will use current working directory of NeoVim
---       -- if "relative" it will use directory where the file is located
---       direction = "horizontal" | "float" -- float will create floating window and horizontal will put it under buffers
---     })
---   end
--- }
+return {
+  -- 'LowByteFox/bun.nvim',
+  -- dependencies = {
+  --   'nvim-treesitter/nvim-treesitter',
+  --   'akinsho/toggleterm.nvim'
+  -- },
+  -- config = function()
+  --   require('bun').setup({
+  --     close_on_exit = true | false, -- if the terminal window should close instantly after bun exited
+  --     cwd = "current" | "relative", -- run_current's working directory
+  --     -- if "current" it will use current working directory of NeoVim
+  --     -- if "relative" it will use directory where the file is located
+  --     direction = "horizontal" | "float" -- float will create floating window and horizontal will put it under buffers
+  --   })
+  -- end
+}
